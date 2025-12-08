842092627d42cdcd23fce3540ce387e4eaddda6f
Victor Monnot <victormonnot@protonmail.com>
2025-12-08 20:11:33 +0100

    feat: Created a c function that measures the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root of the tree
+ *
+ * Return: Size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int pourUnTest = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	pourUnTest = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+
+	return (pourUnTest);
+}
