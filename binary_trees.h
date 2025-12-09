c05622865ea2d64d666e9a4627bad37f05a831e9
JarodLge <contact.jarodlge@gmail.com>
2025-12-09 09:39:15 +0100

    feat: add binary tree depth measurement function and update main for testing

--- binary_trees.h
+++ binary_trees.h
@@ -3,17 +3,18 @@
 #include <stdlib.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 
 typedef struct binary_tree_s
 {
 	int n;
+
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
@@ -21,8 +22,12 @@ typedef struct binary_tree_s
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
+
 #endif
