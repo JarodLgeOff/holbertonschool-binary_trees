188a13737bef217f24357713268973279ba49dcd
JarodLge <contact.jarodlge@gmail.com>
2025-12-09 10:44:03 +0100

    feat: implement binary tree perfection check and update main for testing

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t left_leaves, right_leaves;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+
+	if (left_leaves != right_leaves)
+	{
+		return (0);
+	}
+
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+}
